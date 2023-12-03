fe65489f3eeb4611189131b824e20602c68f4187
josushiman <timurmustafa1989@gmail.com>
2023-12-03 12:45:55 +0000

    first commit

---
+++ app/helpers.py
@@ -0,0 +1,156 @@
+from tortoise.models import Model
+from tortoise.exceptions import IntegrityError, OperationalError, FieldError
+from fastapi import HTTPException
+from uuid import UUID
+from .models import Accounts, AccountTypes, BalanceTransfers, Budgets, Companies, CompanyCategories, DirectDebits, Mortgages, \
+    Projects, ProjectItems, ProjectItemCategories
+
+class ReactAdmin():
+    @classmethod
+    async def get_entity_model(cls, resource: str) -> Model:
+        model_list ={
+            "accounts": Accounts,
+            "account-types": AccountTypes,
+            "balance-transfers": BalanceTransfers,
+            "budgets": Budgets,
+            "companies": Companies,
+            "company-categories": CompanyCategories,
+            "direct-debits": DirectDebits,
+            "mortgages": Mortgages,
+            "projects": Projects,
+            "project-item-categories": ProjectItemCategories,
+            "project-items": ProjectItems
+        }
+
+        try:
+            return model_list[resource]
+        except KeyError:
+            raise HTTPException(status_code=404)
+    
+    @classmethod
+    async def get_one(cls, model: Model, _id: UUID) -> Model:
+        return await model.get(id=_id)
+
+    @classmethod
+    async def get_list(cls, model: Model, commons: dict, kwargs_raw: dict) -> tuple:
+        # When an list of id's are provided, go straight to the get_many function.
+        if 'id' in kwargs_raw and type(kwargs_raw['id']) is list: return await cls.get_many(model, kwargs_raw['id'])
+
+        # Otherwise process the kwargs, limit, and order values
+        kwargs = await cls.process_raw_kwargs(model, kwargs_raw)
+        order_by, limit = await cls.get_order_limit_value(commons)
+
+        # Make the DB requests to get the entities and count
+        entities = await cls.get_entity_list_data(model, limit, commons["_start"], order_by, kwargs if kwargs != {} else None)
+        row_count = await cls.get_entity_list_count(model, kwargs if kwargs != {} else None)
+
+        # Load any reference data if needed.
+        # entities = await cls.load_entities(model, entities)
+
+        # Return it as a tuple.
+        return entities, str(row_count)
+    
+    @classmethod
+    async def get_many(cls, model: Model, ids: list[UUID]) -> tuple:
+        results = []
+        # Get each entity from the id's provided in the list.
+        for entity_id in ids:
+            db_entity = await cls.get_one(model, entity_id)
+            results.append(db_entity)
+
+        return results, str(len(results))
+
+    @classmethod
+    async def create(cls, resource: str, resp_body: dict):
+        # From the resource, get the Model and attempt to create it with kwargs.
+        entity_model = await cls.get_entity_model(resource)
+
+        # Create the entity
+        try:
+            return await entity_model.create(**resp_body)
+        except AttributeError as e_attr:
+            return # May have missed adding '_id' at the end of any FK relations in the json. e.g. type = type_id
+        except IntegrityError as e_dupe:
+            return
+
+    @classmethod
+    async def process_raw_kwargs(cls, model: Model, kwargs_raw: dict):
+        # Only add values which exist from the request
+        kwargs = {}
+        for key, value in kwargs_raw.items():
+            # Only store values which exist
+            if value is not None:
+                # Allow us to search the DB with like values
+                if key == 'name': 
+                    kwargs['name__icontains'] = value
+                # Otherwise just store the value in the new dict
+                else:
+                    kwargs[key] = value
+        return kwargs
+    
+    @classmethod
+    async def get_order_limit_value(cls, commons: dict):
+        order_by = None
+        if commons["_order"] or commons["_sort"]:
+            # Get the sort value
+            order_by = await cls.get_sort_value(commons["_order"], commons["_sort"])
+
+        # Get the limit value
+        limit = commons["_end"] - commons["_start"]
+
+        if limit < 0:
+            raise HTTPException(status_code=400, detail={
+                "message": "End query parameter cannot be less than start query parameter."
+            })
+
+        return order_by, limit
+
+    @classmethod
+    async def get_sort_value(cls, order: str, sort: str) -> str:
+        if order == "ASC":
+            return sort
+        return "-"+sort
+
+    @classmethod
+    async def get_entity_list_data(cls, model: Model, limit: int, offset: int, order_by: str = None, filter: dict = None) -> list[Model]:
+        try:
+            if order_by is None:
+                if filter:
+                    return await model.filter(**filter).limit(limit).offset(offset).all()
+                return await model.all().limit(limit).offset(offset)
+            elif filter is not None:
+                return await model.filter(**filter).limit(limit).offset(offset).order_by(order_by).all()
+            return await model.all().limit(limit).offset(offset).order_by(order_by)
+        except OperationalError as e_opp:
+            raise HTTPException(status_code=422, detail={
+                "message": "Incorrect sort field provided."
+            }) from e_opp
+    
+    @classmethod
+    async def get_entity_list_count(cls, model: Model, filter: dict = None) -> int:
+        if filter is not None:
+            return await model.filter(**filter).count()
+        return await model.all().count()
+
+    @classmethod
+    async def update(cls, resource: str, resp_body: dict, _id: UUID):
+        # From the resource, get the Model and attempt to create it with kwargs.
+        entity_model = await cls.get_entity_model(resource)
+
+        try:
+            return await entity_model.filter(id=_id).update(**resp_body)
+        except FieldError as e_field:
+            print("wrong field entered")
+            return
+        except IntegrityError as e_dupe:
+            print("tried creating a dupe")
+            return
+
+    @classmethod
+    async def delete(cls, resource: str, id: str):
+        entity_model = await cls.get_entity_model(resource)
+
+        deleted_count = await entity_model.filter(id=id).delete()
+        if not deleted_count:
+            print("didnt find entity")
+        return
---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+from dotenv import load_dotenv
+from typing import List
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Response, Depends, Query, Request
+from tortoise import Tortoise
+from uuid import UUID
+from app.helpers import ReactAdmin as ra
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+    dotenv_db_url = os.getenv("DB_URL")
+    await Tortoise.init(
+        db_url=dotenv_db_url,
+        modules={'models': ['app.models']}
+    )
+    # Generate the model schemas.
+    await Tortoise.generate_schemas()
+    yield
+    # Close all connections when shutting down.
+    await Tortoise.close_connections()
+
+app = FastAPI(lifespan=lifespan)
+
+async def common_parameters(_end: int = 10, _start: int = 0, _order: str = Query(default="ASC", min_length=3, max_length=4, regex="ASC|DESC"), \
+    _sort: str = None):
+    return {"_end": _end, "_start": _start, "_order": _order, "_sort": _sort}
+
+@app.post("/{resource}", status_code=201)
+async def create(resource: str, _body: dict):
+    return await ra.create(resource, _body)
+
+@app.get("/{resource}/{_id}")
+async def get_one(resource: str, _id: UUID):
+    # Ensure a model exists for the resource that's been passed.
+    entity_model = await ra.get_entity_model(resource)
+
+    return await ra.get_one(entity_model, _id)
+
+@app.get("/{resource}")
+async def get_list(request: Request, response: Response, resource: str, commons: dict = Depends(common_parameters), \
+    _id: list[UUID] | None = Query(default=None, alias="id")):
+    
+    # Instansiate the kwargs object, incase no kwargs are passed
+    kwargs = {}
+    # Iterate through the query parameters
+    for query, value in request.query_params.items():
+        # Skip any that are in commons, as well as if they are "id"
+        if query not in commons.keys() and query != 'id':
+            kwargs[query] = value
+        # This can sometimes be a list of id's so we want to capture all of them in a list.
+        elif query == 'id':
+            kwargs['id'] = _id
+
+    # Ensure a model exists for the resource that's been passed.
+    entity_model = await ra.get_entity_model(resource)
+
+    # Get the entities and the count.
+    entities, count = await ra.get_list(entity_model, commons, kwargs)
+
+    # List responses require the count to be set in the header using a custom param.
+    response.headers["X-Total-Count"] = count
+    return entities
+
+@app.put("/{resource}/{_id}")
+async def update(resource: str, _body: dict, _id: UUID):
+    return await ra.update(resource, _body, _id)
+
+@app.delete("/{resource}/{_id}", status_code=204)
+async def delete(resource: str, _id: UUID):
+    return await ra.delete(resource, _id)
+
+@app.delete("/{resource}", status_code=204)
+async def delete_many(resource: str, _ids: list[UUID] = Query(default=None, alias="ids")):
+    for _id in _ids:
+        await ra.delete(resource, _id)
+    return #add log here to say done or something
