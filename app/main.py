01bad78c661d2b82cf490c947d9e13fe0bd60ea9
josushiman <timurmustafa1989@gmail.com>
2023-12-16 15:21:58 +0000

    handling origin headers

--- app/main.py
+++ app/main.py
@@ -48,26 +48,30 @@ async def lifespan(app: FastAPI):
 
 async def get_token_header(request: Request, x_token: str = Header(...)):
     if dotenv_origins != ['*'] or dotenv_hosts != ['*']:
-        logging.info(request.headers.raw)
+        logging.debug(request.headers.raw)
         try:
-            origin = request.headers["origin"]
             referer = request.headers["referer"]
             host = request.headers["host"]
-            # logging.info(f"Origin: {origin}")
-            # logging.info(f"Referer: {referer}")
-            # logging.info(f"Host: {host}")
-            if origin != dotenv_origins:
-                logging.warning(f"Origin {origin} attempted access using a valid token")
-                raise HTTPException(status_code=403)
             if referer != dotenv_referer:
+                logging.debug(f"Referer: {referer}")
                 logging.warning(f"Referer {referer} attempted access using a valid token")
                 raise HTTPException(status_code=403)
             if dotenv_hosts != host:
+                logging.debug(f"Host: {host}")
                 logging.warning(f"Host {host} attempted access using a valid token")
                 raise HTTPException(status_code=403)
         except KeyError as e_key:
-            logging.warning(f"Attempt to access API from unauthorised location", exc_info=e_key)
+            logging.warning(f"Either Referer or Host was not set", exc_info=e_key)
             raise HTTPException(status_code=403)
+        
+        try:
+            origin = request.headers["origin"]
+            if origin != dotenv_origins:
+                logging.debug(f"Origin: {origin}")
+                logging.warning(f"Origin {origin} attempted access using a valid token")
+                raise HTTPException(status_code=403)
+        except KeyError as e_key:
+            logging.warning(f"Origin was not set for {request.headers['host']}")
 
     if x_token != dotenv_token:
         logging.warning(f"Invalid token provided from Origin and/or Host")
