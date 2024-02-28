9602f0021cd8586907d2b97894db0fb50ebf873e
josushiman <timurmustafa1989@gmail.com>
2024-02-28 21:30:56 +0000

    x_token is a UUID not a str, type it correctly

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ async def lifespan(app: FastAPI):
     logging.info("Shutting down application.")
     await Tortoise.close_connections()
 
-async def get_token_header(request: Request, x_token: str = Header(...)):
+async def get_token_header(request: Request, x_token: UUID = Header(...)):
     if dotenv_origins != ['*'] or dotenv_hosts != ['*']:
         logging.debug(request.headers.raw)
         try:
